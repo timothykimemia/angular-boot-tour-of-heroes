b7b8dc2ca1938df0f0375b059ac3398ada3252dd
Timothy Kimemia <timothykimemia01@gmail.com>
2020-09-22 19:05:44 +0300

    Tutorial on Angular 9 'TourOfHeroes'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './heroes/hero-detail/hero-detail.component';
+import {PageNotFoundComponent} from './dashboard/page-not-found/page-not-found.component';
+import { CrisisListComponent } from './heroes/crisis-list/crisis-list.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'crisis', component: CrisisListComponent },
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'heroes/:id', component: HeroDetailComponent },
+
+  // Always to be placed at last position
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(
+    routes,
+    { enableTracing: false } // <-- debugging purposes only
+  )],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './heroes/hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { MessageComponent } from './messages/message/message.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroSearchComponent } from './heroes/hero-search/hero-search.component';
+import { PageNotFoundComponent } from './dashboard/page-not-found/page-not-found.component';
+import { CrisisListComponent } from './heroes/crisis-list/crisis-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    MessageComponent,
+    DashboardComponent,
+    HeroSearchComponent,
+    PageNotFoundComponent,
+    CrisisListComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule,
+
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
